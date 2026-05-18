dc7d31d0a8fb42fe0d7208e94bf042cc51cb4b87
issam09932 <ouladsmaneissam2006@icloud.com>
2026-05-18 17:50:25 +0100

    tali

--- frontend_ticket_gestion/src/components/ui/DashboardContent.jsx
+++ frontend_ticket_gestion/src/components/ui/DashboardContent.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ManagerDaschboard from './Dashboard/ManagerDaschboard';
+import ManagerDaschboard from './Dashboard/ManagerDaschboard';//STUB - 
 import Meetings from './DashboardComposentes/Meetings';
 import Messages from './DashboardComposentes/Messages';
 import TicketsContents from './DashboardComposentes/Tickets/TicketsContents';
 import Contacts from './DashboardComposentes/Contacts';
 import CreateTicket from './DashboardComposentes/CreateTicket';
 import PKIDashboard from './Dashboard/PKIDashboard';
-import SDHomeDashboard from './Dashboard/SDHomeDashboard';
+import SDHomeDashboard from './Dashboard/SDHomeDashboard';//STUB -
 
 
 function DashboardContent({ activeItem, contentOverride, onViewAllTickets, onMessages }) {
@@ -27,6 +27,7 @@ function DashboardContent({ activeItem, contentOverride, onViewAllTickets, onMes
       return <Meetings />;
     case 'Dashboard':
     default:
+      return <PKIDashboard onViewAllTickets={onViewAllTickets} onMessages={onMessages} />;   
   }
 }
 
